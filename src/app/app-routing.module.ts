d0991fa37b09bd08bd96b3ffecb1323ca6c7cf98
brandonwrd <wardesque@gmail.com>
2022-10-10 19:55:12 -0400

    added paths/routes to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { NetflixComponent } from './netflix/netflix.component';
+import { HuluComponent } from './hulu/hulu.component';
+import { PrimeComponent } from './prime/prime.component';
+import { HboComponent } from './hbo/hbo.component';
+import { PeacockComponent } from './peacock/peacock.component';
 
-
+const routes: Routes = [
+  { path: 'netlfix', component: NetflixComponent },
+  { path: 'hulu', component: HuluComponent },
+  { path: 'prime', component: PrimeComponent },
+  { path: 'hbo', component: HboComponent },
+  { path: 'peacock', component: PeacockComponent }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
