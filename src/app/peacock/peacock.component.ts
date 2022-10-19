a9dfb12a1527256c3cff59ca90a7794ab489d7ca
brandonwrd <wardesque@gmail.com>
2022-10-19 19:43:26 -0400

    added shows to array in ts file and used ngfor to display them

--- src/app/peacock/peacock.component.ts
+++ src/app/peacock/peacock.component.ts
@@ -8,7 +8,21 @@ import { PeacockService } from '../peacock.service';
   styleUrls: ['./peacock.component.css']
 })
 export class PeacockComponent implements OnInit {
-  peacockShows: Peacock[] = [];
+  peacockShows: Peacock[] = [
+  
+  {shows: "A.P. Bio"},
+  {shows: "Angelyne"},
+  {shows: "Bates Motel"},
+  {shows: "Battlestar Galactica"},
+  {shows: "Brooklyn Nine-Nine"},
+  {shows: "Cheers"},
+  {shows: "Chucky"},
+  {shows: "Columbo"},
+  {shows: "Dr. Death"},
+  {shows: "Everybody Hates Chris"}
+
+];
+
 
   constructor(private peacockService: PeacockService) { }
 
