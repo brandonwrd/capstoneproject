19297e65bbd628b536afa836ca4629f04774f3a7
brandonwrd <wardesque@gmail.com>
2022-10-19 19:29:18 -0400

    added hardcoded shows into an array in ts file and used ngfor to display info in html file

--- src/app/hbo/hbo.component.ts
+++ src/app/hbo/hbo.component.ts
@@ -8,7 +8,21 @@ import { HboService } from '../hbo.service';
   styleUrls: ['./hbo.component.css']
 })
 export class HboComponent implements OnInit {
-  hboShows: Hbo[] = [];
+  hboShows: Hbo[] = [
+
+  {shows: "Los Espookys"},
+  {shows: "House of the Dragon"},
+  {shows: "Abbott Elementary"},
+  {shows: "Selena + Chef"},
+  {shows: "Industry"},
+  {shows: "Pretty Little Liars: Original Sin"},
+  {shows: "Genndy Tartakovsky's Primal"},
+  {shows: "The Rehearsal"},
+  {shows: "Westworld"},
+  {shows: "Irma Vep"}
+  
+];
+
 
   constructor(private hboService: HboService) { }
 
