1b3e322eed04c5491ec3ebb3df244e7269e21455
brandonwrd <wardesque@gmail.com>
2022-10-19 19:37:27 -0400

    added hardcoded shows to prime ts file and used ngfor to display them in html file

--- src/app/prime/prime.component.ts
+++ src/app/prime/prime.component.ts
@@ -8,7 +8,20 @@ import { PrimeService } from '../prime.service';
   styleUrls: ['./prime.component.css']
 })
 export class PrimeComponent implements OnInit {
-  primeShows: Prime[] = [];
+  primeShows: Prime[] = [
+  
+  {shows: "Fairfax"},
+  {shows: "Jungle"},
+  {shows: "Yellowjackets"},
+  {shows: "A Private Affair"},
+  {shows: "Death Note"},
+  {shows: "The Lord of the Rings: The Rings of Power"},
+  {shows: "Paper Girls"},
+  {shows: "Panic"},
+  {shows: "A League of Their Own"},
+  {shows: "Comrade Detective"}
+
+];
 
   constructor(private primeService: PrimeService) { }
 
